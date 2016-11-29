e0f0f9f7cf560375a0ee4b78e9f9956d02712051
Chandra Shekhar <shekharindian099@gmail.com>
2016-11-29 15:16:02 +0530

    change detection strategy to onPush

--- src/app/components/trips/trips-list/trips-list.component.ts
+++ src/app/components/trips/trips-list/trips-list.component.ts
@@ -4,12 +4,13 @@ import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 import { TripsService } from './../../../services/trips.service';
 import { Trip } from './../../../models/trip';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'tr-trips-list',
   templateUrl: './trips-list.component.html',
-  styleUrls: ['./trips-list.component.css']
+  styleUrls: ['./trips-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TripsListComponent implements OnInit {
 
