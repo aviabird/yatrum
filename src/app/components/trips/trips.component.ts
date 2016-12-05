fc5392262bba7824f0cf2fba65d73879865f156d
Chandra Shekhar <shekharindian099@gmail.com>
2016-12-05 14:14:41 +0530

    proper indentation

--- src/app/components/trips/trips.component.ts
+++ src/app/components/trips/trips.component.ts
@@ -4,13 +4,13 @@ import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'tr-trips',
-  templateUrl: './trips.component.html',
-  styleUrls: ['./trips.component.css']
+	selector: 'tr-trips',
+	templateUrl: './trips.component.html',
+	styleUrls: ['./trips.component.css']
 })
 export class TripsComponent implements OnInit {
-  constructor(private store: Store<fromRoot.State>) { }
-  ngOnInit() {
-    this.store.dispatch(new LoadTripsAction);    
-  }
+	constructor(private store: Store<fromRoot.State>) { }
+	ngOnInit() {
+		this.store.dispatch(new LoadTripsAction);    
+	}
 }
