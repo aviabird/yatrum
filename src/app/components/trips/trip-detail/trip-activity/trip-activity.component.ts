b54a8b44623a49bada7de82ed1bd7badf171b827
Chandra Shekhar <shekharindian099@gmail.com>
2016-12-16 12:28:16 +0530

    environment file deleted and add it to gitignore

--- src/app/components/trips/trip-detail/trip-activity/trip-activity.component.ts
+++ src/app/components/trips/trip-detail/trip-activity/trip-activity.component.ts
@@ -8,28 +8,28 @@ import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'tr-trip-activity',
-  templateUrl: './trip-activity.component.html',
-  styleUrls: ['./trip-activity.component.css']
+	selector: 'tr-trip-activity',
+	templateUrl: './trip-activity.component.html',
+	styleUrls: ['./trip-activity.component.css']
 })
 export class TripActivityComponent implements OnInit {
-  selectedCity$: Observable<City>;
-  cities$: Observable<City[]>;
-  selectedTrip$: Observable<any>;
-  instagramMedia$: Observable<string[]>;
+	selectedCity$: Observable<City>;
+	cities$: Observable<City[]>;
+	selectedTrip$: Observable<any>;
+	instagramMedia$: Observable<string[]>;
 
 
-  constructor(private store: Store<fromRoot.State>) {
-    this.selectedTrip$ = this.store.select(fromRoot.getSelectedTrip);
-    this.cities$ = this.store.select(fromRoot.getCitiesFromTrip);
-    this.selectedCity$ = this.store.select(fromRoot.getSelectedCity);
-    this.instagramMedia$ = this.store.select(fromRoot.getInstagramMedia);
-  }
-  
+	constructor(private store: Store<fromRoot.State>) {
+		this.selectedTrip$ = this.store.select(fromRoot.getSelectedTrip);
+		this.cities$ = this.store.select(fromRoot.getCitiesFromTrip);
+		this.selectedCity$ = this.store.select(fromRoot.getSelectedCity);
+		this.instagramMedia$ = this.store.select(fromRoot.getInstagramMedia);
+	}
+
 	getUserInstagramMedia() {
 		this.store.dispatch(new LoadMediaAction());
 	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,3 +1,4 @@
+import { environment as env } from './../../../environments/environment';
 import { ServerAuthService } from './../../services/server-auth.service';
 import { LoginAction, LogoutAction, ServerLoginAction, ServerLoginSuccessAction, ServerLogoutAction } from './../../actions/user-auth.action';
 import { UserProfile } from './../../models/user-profile';
@@ -11,55 +12,51 @@ import "rxjs/add/operator/let";
 
 
 @Component({
-  selector: 'tr-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'tr-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  user$: Observable<UserProfile>;
-  authentication$: Observable<any>;
-  CLIENT_ID: string = "865ffa528b874fc3b755ee13b9a79037";
-  CLIENT_SECRET: string = "72e404fed36d4d779a0fad1411e9e486";
-  REDIRECT_URI: string = "http://localhost:4200/instagram_authentication_callback_url";
+	user$: Observable<UserProfile>;
+	authentication$: Observable<any>;
 
+	constructor(private af: AngularFire,
+		private store: Store<fromRoot.State>,
+		private serverAuthService: ServerAuthService) {
+		this.user$ = this.store.let(fromRoot.getUserProfile);
+		this.authentication$ = this.store.let(fromRoot.getAuthStatus);
 
-  constructor(private af: AngularFire, 
-              private store: Store<fromRoot.State>,
-              private serverAuthService: ServerAuthService) {
-    this.user$ = this.store.let(fromRoot.getUserProfile);
-    this.authentication$ = this.store.let(fromRoot.getAuthStatus);
-    
-    // Check if the user is already logged in
-    let user_data = JSON.parse(localStorage.getItem('user'));
-    if (user_data) {
-      console.log('token is', user_data.auth_token);
-      console.log('user is', user_data.user);
-      let user = this.serverAuthService.getLoggedInUser(user_data.auth_token);
-      user.subscribe(
-        // Dispatch login success when the we get the user object
-        data => this.store.dispatch(new ServerLoginSuccessAction(data))
-        // if required do anyredirects after this
-      )
-    }
-  }
+		// Check if the user is already logged in
+		let user_data = JSON.parse(localStorage.getItem('user'));
+		if (user_data) {
+			console.log('token is', user_data.auth_token);
+			console.log('user is', user_data.user);
+			let user = this.serverAuthService.getLoggedInUser(user_data.auth_token);
+			user.subscribe(
+				// Dispatch login success when the we get the user object
+				data => this.store.dispatch(new ServerLoginSuccessAction(data))
+				// if required do anyredirects after this
+			)
+		}
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
-  login() {
-    // Firebase authentication
-    // this.store.dispatch(new LoginAction);
-    // this.store.dispatch(new ServerLoginAction);
-  }
+	login() {
+		// Firebase authentication
+		// this.store.dispatch(new LoginAction);
+		// this.store.dispatch(new ServerLoginAction);
+	}
 
-  logout() {
-    // Firebase authentication    
-    // this.store.dispatch(new LogoutAction);
-    this.store.dispatch(new ServerLogoutAction);
-  }
+	logout() {
+		// Firebase authentication    
+		// this.store.dispatch(new LogoutAction);
+		this.store.dispatch(new ServerLogoutAction);
+	}
 
-  onLoginWithInstagram() {
-    window.location.href = `https://api.instagram.com/oauth/authorize/?client_id=${this.CLIENT_ID}&redirect_uri=${this.REDIRECT_URI}&response_type=code`;
-}
+	onLoginWithInstagram() {
+		window.location.href = `https://api.instagram.com/oauth/authorize/?client_id=${env.CLIENT_ID}&redirect_uri=${env.REDIRECT_URI}&response_type=code`;
+	}
 
 }
