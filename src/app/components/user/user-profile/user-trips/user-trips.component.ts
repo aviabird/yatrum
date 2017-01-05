2a48683eab5a6d6c16d4680e3745cbfb3f898179
Chandra Shekhar <shekharindian099@gmail.com>
2017-01-05 12:04:39 +0530

    update profile pic button

--- src/app/components/user/user-profile/user-trips/user-trips.component.ts
+++ src/app/components/user/user-profile/user-trips/user-trips.component.ts
@@ -1,3 +1,4 @@
+import { UserProfile } from './../../../../models/user-profile';
 import * as fromRoot from './../../../../reducers/index';
 import { Store } from '@ngrx/store';
 import { Trip } from './../../../../models/trip';
@@ -11,8 +12,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserTripsComponent implements OnInit {
   userTrips$: Observable<Trip[]>;
+  public user$: Observable<UserProfile>;
 
   constructor(private store: Store<fromRoot.State>) {
+    this.user$ = this.store.let(fromRoot.getUserProfile);
     this.userTrips$ = this.store.select(fromRoot.getUserTripsCollection);
    }
 
