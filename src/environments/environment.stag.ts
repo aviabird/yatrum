3f604af6be6c54848d412c18f6635644e9d66dcd
Ashish Singh <aitashish173@gmail.com>
2017-02-11 21:43:37 +0530

    added staging env file

---
+++ src/environments/environment.stag.ts
@@ -0,0 +1,8 @@
+export const environment = {
+   production: true,
+   CLIENT_ID: "865ffa528b874fc3b755ee13b9a79037",
+   API_ENDPOINT: "https://travel-api.herokuapp.com/", // add prod api link
+   REDIRECT_URI: "https://aviabird.github.io/travel-app/instagram_authentication_callback_url", // add prod redirect url
+   CLOUDINARY_CLOUD_NAME: 'zeus999',
+   CLOUDINARY_UPLOAD_PRESET: 'blxtvppn'
+};
