647b8f37cb83f8648d5a16bec87c41cd6901ea21
Chandra Shekhar <shekharindian099@gmail.com>
2017-02-14 13:38:12 +0530

    add trending trips css

--- src/app/components/dashboard/trending-trips/trending-trips.component.ts
+++ src/app/components/dashboard/trending-trips/trending-trips.component.ts
@@ -1,3 +1,9 @@
+import { getTrendingTrips, State } from './../../../reducers/index';
+import { LoadMoreTripsAction, LoadTrendingTripsAction } from './../../../actions/trips.action';
+import { TripsService } from './../../../services/trips.service';
+import { Store } from '@ngrx/store';
+import { Trip } from './../../../models/trip';
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrendingTripsComponent implements OnInit {
 
-  constructor() { }
+  trendingTrips$: Observable<Trip[]>;
+  private page: number = 1;
+
+  constructor(private store: Store<State>, private tripService: TripsService) {
+    this.trendingTrips$ = this.store.select(getTrendingTrips);
+    this.trendingTrips$.subscribe((trips) => {
+      console.log("trips", trips);
+    })
+  }
 
   ngOnInit() {
+    this.store.dispatch(new LoadTrendingTripsAction({page: 1, tripsType: "feeds"}));
+  }
+
+  onScroll() {
+    this.page++;
+    let total_pages = this.tripService.total_pages;
+    if(this.page <=  total_pages)
+      this.store.dispatch(new LoadMoreTripsAction({page: this.page, tripsType: "feeds"}));
   }
 
 }
