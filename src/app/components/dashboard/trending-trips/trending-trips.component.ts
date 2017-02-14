475f2530433d809f344dddd9f60bff7e97536742
Chandra Shekhar <shekharindian099@gmail.com>
2017-02-14 14:07:27 +0530

    add trending trips

--- src/app/components/dashboard/trending-trips/trending-trips.component.ts
+++ src/app/components/dashboard/trending-trips/trending-trips.component.ts
@@ -4,12 +4,28 @@ import { TripsService } from './../../../services/trips.service';
 import { Store } from '@ngrx/store';
 import { Trip } from './../../../models/trip';
 import { Observable } from 'rxjs/Observable';
-import { Component, OnInit } from '@angular/core';
+import { Component, 
+  OnInit, 
+  trigger,
+  state,
+  transition,
+  style,
+  animate  
+} from '@angular/core';
 
 @Component({
   selector: 'tr-trending-trips',
   templateUrl: './trending-trips.component.html',
-  styleUrls: ['./trending-trips.component.scss']
+  styleUrls: ['./trending-trips.component.scss'],
+    animations: [
+    trigger('fadeIn', [
+      state('in', style({ opacity: 1 })),
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(500)
+      ])
+    ])
+  ]
 })
 export class TrendingTripsComponent implements OnInit {
 
