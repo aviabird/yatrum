3871870a5a3bebef8f73f62dd6e37a653f9332ba
Chandra Shekhar <shekharindian099@gmail.com>
2017-02-14 13:25:26 +0530

    add trending trips route

--- src/app/components/dashboard/dashboard.routes.ts
+++ src/app/components/dashboard/dashboard.routes.ts
@@ -1,5 +1,7 @@
+import { TrendingTripsComponent } from './trending-trips/trending-trips.component';
 import { FeedTripsComponent } from './feed-trips/feed-trips.component';
 export const DashboardRoutes = [
 	{ path: '', redirectTo: 'feeds', pathMatch: 'full' }, 
-	{ path: 'feeds', component: FeedTripsComponent }
+	{ path: 'feeds', component: FeedTripsComponent },
+	{ path: 'trending', component: TrendingTripsComponent}
 ]
