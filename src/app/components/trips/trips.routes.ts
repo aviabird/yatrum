e27de98d4c965fd9302b45fb31b9131b4f93b382
Chandra Shekhar <shekharindian099@gmail.com>
2017-02-14 14:59:16 +0530

    trips routes fixes

--- src/app/components/trips/trips.routes.ts
+++ src/app/components/trips/trips.routes.ts
@@ -1,16 +1,11 @@
 import { CanActivateViaAuthGuard } from './../../guards/auth.guard';
 import { TripEditComponent } from './trip-edit/trip-edit.component';
 import { TripsResolveGuard } from './../../guards/trips-resolve.guard';
-import { TripsListComponent } from './trips-list/trips-list.component';
 import { TripsComponent } from './trips.component';
 import { Routes } from '@angular/router';
 import { TripDetailComponent } from './trip-detail/trip-detail.component';
 
 export const TripRoutes = [
-  { 
-    path: '', 
-    component: TripsListComponent
-  },
   {
     path: 'new',
     component: TripEditComponent,
