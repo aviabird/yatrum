97898c107c738bf8a6b3f54412a2f2c2f4be160f
voidzero <kartikjagdale11@gmail.com>
2017-03-02 18:35:56 +0530

    change routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
+import { SharedModule } from './shared/index';
 import { AmbassadorComponent } from './components/misc/ambassador/ambassador.component';
 import { SignupComponent } from './shared/auth/signup/signup.component';
-import { LoginComponent } from './shared/auth/login/login.component';
 import { InstagramAuthenticationCallbackComponent } from './shared/instagram-authentication-callback/instagram-authentication-callback.component';
 import { RouterModule, Routes } from "@angular/router";
-
 
 export const routes: Routes = [
 
 	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-	{ path: 'login', component: LoginComponent },
-	{ path: 'signup', component: SignupComponent},
+	{ path: 'auth', loadChildren: './shared/auth/auth.module#AuthModule'},
 	{ path: 'instagram_authentication_callback_url', component: InstagramAuthenticationCallbackComponent },
 	{ path: 'ambassadors', component: AmbassadorComponent}
 ];
