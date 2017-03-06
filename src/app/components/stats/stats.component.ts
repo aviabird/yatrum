4fe3a2d4cfae7937aed65420a51f304867e2a21b
voidzero <kartikjagdale11@gmail.com>
2017-03-06 22:31:37 +0530

    implemented stats feature

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { Response } from '@angular/http';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
@@ -8,6 +8,7 @@ import * as fromRoot from './../../reducers/index';
 import { LoadUserTripsAction } from './../../actions/trips.action';
 import { Trip } from './../../models/trip';
 import { TripsService } from './../../services/trips.service';
+import { UIChart } from "primeng/components/chart/chart";
 
 @Component({
   selector: 'tr-stats',
@@ -16,24 +17,50 @@ import { TripsService } from './../../services/trips.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StatsComponent implements OnInit, OnDestroy {
+
+  @ViewChild("chart") chart;
+
   private subscription: Subscription;
   userTrips$: Observable<Trip[]>;
   userIndex: string;
-  data: Object = {};
-  
+  data: any = {
+              labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+              datasets: [
+                {
+                  label: "Dummy Data",
+                  backgroundColor: '#42A5F5',
+                  borderColor: '#1E88E5',
+                  data:  [28, 48, 40, 19, 86, 27, 90]
+                }
+              ]
+            }
+
   constructor(
     private store: Store<fromRoot.State>,
     private route: ActivatedRoute,
     private tripService: TripsService
   ) {
     this.userTrips$ = this.store.select(fromRoot.getUserTripsCollection);
-    
-    // Set Grph Data for First Trip
+
+    /**Set Graph Data for First Trip */
     this.userTrips$.subscribe(trips => {
-      if(trips && trips.length > 0){
+      if (trips && trips.length > 0) {
         let trip = trips[0];
         this.tripService.get_graph_data_for_trip(trip.id).subscribe(response => {
-          this.set_graph_data(trip.name, response.labels, response.series)          
+          // this.set_graph_data(trip.name, response.labels, response.series, this.chart)
+          if(response) {
+            this.data = {
+              labels: response.labels,
+              datasets: [
+                {
+                  label: trip.name,
+                  backgroundColor: '#42A5F5',
+                  borderColor: '#1E88E5',
+                  data: response.series
+                }
+              ]
+            }
+          }
         })
       }
     })
@@ -50,29 +77,21 @@ export class StatsComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  getGraphDataForTrip(id, name){
+  getGraphDataForTrip(id, name, chart) {
     this.tripService.get_graph_data_for_trip(id).subscribe(
-    response => {
-      this.set_graph_data(name, response.labels, response.series)
-    }, 
-    err => {
-      alert("Something went wrong");
-    })
+      response => {
+        this.set_graph_data(name, response.labels, response.series, chart)
+      },
+      err => {
+        alert("Something went wrong");
+      })
   }
 
-
-  set_graph_data(label_name, labels, data) {
-    this.data = {
-      labels: labels, // ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: label_name,
-          backgroundColor: '#42A5F5',
-          borderColor: '#1E88E5',
-          data: data //[65, 59, 80, 81, 56, 55, 40]
-        }
-      ]
-    }
-    console.log("Inside Set data and data is", this.data )
+  /**Sets Graph Data */
+  set_graph_data(label_name, labels, data, chart) {
+    this.data.labels = labels;
+    this.data.datasets[0].data = data;
+    this.data.datasets[0].label = label_name;
+    chart.refresh();
   }
 }
