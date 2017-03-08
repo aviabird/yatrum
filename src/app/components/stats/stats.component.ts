c920717503c65924795572b81d2914e7f5b7685d
voidzero <kartikjagdale11@gmail.com>
2017-03-08 17:01:57 +0530

    made stats graph view as medium

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -9,108 +9,161 @@ import { LoadUserTripsAction } from './../../actions/trips.action';
 import { Trip } from './../../models/trip';
 import { TripsService } from './../../services/trips.service';
 import { UIChart } from "primeng/components/chart/chart";
+import * as moment from 'moment/moment';
 
 @Component({
-    selector: 'tr-stats',
-    templateUrl: './stats.component.html',
-    styleUrls: ['./stats.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'tr-stats',
+  templateUrl: './stats.component.html',
+  styleUrls: ['./stats.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StatsComponent implements OnInit, OnDestroy {
 
-    @ViewChild("chart") chart: UIChart;
+  @ViewChild("chart") chart: UIChart;
 
-    dummyData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-            {
-                label: "Dummy Data",
+  dummyData = {
+    labels: ['', '', 'March', '', '', 'June', 'July', '', '', '', 'December'],
+    datasets: [
+      {
+        label: "Dummy Data",
+        backgroundColor: 'rgba(2,184,117,.8)',
+        borderColor: '#1E88E5',
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }
+    ]
+  }
+  private subscription: Subscription;
+  userTrips$: Observable<Trip[]>;
+  userIndex: string;
+  data = new Subject();
+  options = {
+    title: {
+      display: true,
+      fontSize: 20,
+      fontColor: 'black',
+      padding: 15,
+      text: "Trip Stats"
+    },
+
+    scales: {
+      xAxes: [{
+        // gridLines: {
+        //   // You can change the color, the dash effect, the main axe color, etc.
+        //   borderDash: [1, 1],
+        //   color: "white"
+        // },
+        ticks: {
+          autoSkip: false,
+          // maxRotation: 180,
+          minRotation: 360
+        }
+      }],
+      // And this will affect the horizontal lines (yAxe) of your dataset
+      yAxes: [{
+        // gridLines: {
+        //   borderDash: [1, 1],
+        //   color: "black"
+        // }
+        // ticks: {
+        //   stepSize: 1,
+        // }
+      }]
+    },
+    responsive: false,
+    maintainAspectRatio: false,
+    // multiTooltipTemplate : "<%%=datasetLabel%> : <%%=value%>"
+  };
+
+  dateRange = {
+    start_date: moment(new Date).subtract(30, 'days'),
+    end_date: moment(new Date)
+  };
+
+  constructor(
+    private store: Store<fromRoot.State>,
+    private route: ActivatedRoute,
+    private tripService: TripsService
+  ) {
+
+    this.data.subscribe(response => {
+      if (this.chart && response) {
+        setTimeout(() => this.chart.refresh(), 10);
+      }
+    })
+  }
+
+  ngOnInit() {
+    this.subscription = this.route.parent.params.subscribe(
+      (params) => this.userIndex = params['id']
+    )
+    this.store.dispatch(new LoadUserTripsAction(this.userIndex));
+    this.setInitialGraphData();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.data.unsubscribe();
+  }
+
+  formatLabels(labels: Array<any>) {
+    labels = labels.map((label, i) => {
+      if (i % 7 == 0) {
+        return moment(label).format("MMMM Do");
+      } else {
+        return ""
+      }
+    })
+    return labels;
+  }
+
+
+  setInitialGraphData() {
+    /**Set Graph Data for First Trip */
+    this.userTrips$ = this.store.select(fromRoot.getUserTripsCollection).do(trips => {
+      if (trips && trips.length > 0) {
+        let trip = trips[0];
+        this.tripService.get_graph_data_for_trip(trip.id).subscribe(response => {
+          let temp = {
+            labels: this.formatLabels(response.labels),
+            datasets: [
+              {
+                label: trip.name,
                 backgroundColor: 'rgba(2,184,117,.8)',
                 borderColor: '#1E88E5',
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-    }
-    private subscription: Subscription;
-    userTrips$: Observable<Trip[]>;
-    userIndex: string;
-    data = new Subject();
-
-    constructor(
-        private store: Store<fromRoot.State>,
-        private route: ActivatedRoute,
-        private tripService: TripsService
-    ) {
-
-        this.data.subscribe(response => {
-            if (this.chart && response) {
-                setTimeout(() => this.chart.refresh(), 10);
-            }
-        })
-    }
-
-
-    ngOnInit() {
-        this.subscription = this.route.parent.params.subscribe(
-            (params) => this.userIndex = params['id']
-        )
-        this.store.dispatch(new LoadUserTripsAction(this.userIndex));
-        this.setInitialGraphData();
-    }
-
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
-        this.data.unsubscribe();
-    }
-
-
-    setInitialGraphData() {
-        /**Set Graph Data for First Trip */
-        this.userTrips$ = this.store.select(fromRoot.getUserTripsCollection).do(trips => {
-            if (trips && trips.length > 0) {
-                let trip = trips[0];
-                this.tripService.get_graph_data_for_trip(trip.id).subscribe(response => {
-                    let temp = {
-                        labels: response.labels,
-                        datasets: [
-                            {
-                                label: trip.name,
-                                backgroundColor: 'rgba(2,184,117,.8)',
-                                borderColor: '#1E88E5',
-                                data: response.series
-                            }
-                        ]
-                    }
-                    // Service call end
-                    this.data.next(temp);
-                })
-            }
-        })
-    }
-
-    getGraphDataForTrip(id, name) {
-        this.tripService.get_graph_data_for_trip(id).subscribe(
-            response => {
-                let temp = this.set_graph_data(name, response.labels, response.series);
-                this.data.next(temp)
-            },
-            err => {
-                alert("Something went wrong");
-            })
-    }
-
-    /**Sets Graph Data */
-    set_graph_data(label_name, labels, data) {
-        return {
-            labels: labels,
-            datasets: [
-                {
-                    label: label_name,
-                    backgroundColor: 'rgba(2,184,117,.8)',
-                    borderColor: '#1E88E5',
-                    data: data
-                }
+                data: response.series
+              }
             ]
+          }
+          // Service call end
+          this.data.next(temp);
+        })
+      }
+    })
+  }
+
+  getGraphDataForTrip(id, name) {
+    this.tripService.get_graph_data_for_trip(id).subscribe(
+      response => {
+        let temp = this.set_graph_data(name, response.labels, response.series);
+        this.data.next(temp)
+      },
+      err => {
+        alert("Something went wrong");
+      })
+  }
+
+  /**Sets Graph Data */
+  set_graph_data(label_name, labels, data) {
+    return {
+      labels: this.formatLabels(labels),
+      datasets: [
+        {
+          label: label_name,
+          backgroundColor: 'rgba(2,184,117,.8)',
+          borderColor: '#1E88E5',
+          data: data
         }
+      ]
     }
+  }
 }
